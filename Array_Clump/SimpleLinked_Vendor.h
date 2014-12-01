0d17b9f23462bf5e94e7a2a7d32d8f6cbdd35d93
Shoeboxam <shoeboxam@gmail.com>
2014-12-01 16:23:16 -0600

    Vendorlist class

---
+++ Array_Clump/SimpleLinked_Vendor.h
@@ -0,0 +1,181 @@
+#ifndef SIMPLELINKED_VENDOR_H
+#define SIMPLELINKED_VENDOR_H
+
+
+#include "SimpleLinked.h" //Same file from part B
+#include <iostream>
+#include <fstream>
+
+#include <string>
+#include <vector>
+using std::string;
+using std::vector;
+
+struct Vendor {
+
+	Vendor(){}
+
+	int _ID;
+	string _name, _city, _state, _zip;
+
+	bool operator<(Vendor& input){
+		return _ID < input._ID;
+	}
+
+	bool operator>(Vendor& input){
+		return _ID > input._ID;
+	}
+
+	friend ostream& operator<<(ostream& base, Vendor& input){
+		base << endl
+			<< " --------------------" << endl
+			<< " ID    " << input._ID << endl
+			<< " Name  " << input._name << endl
+			<< " City  " << input._city << endl
+			<< " State " << input._state << endl
+			<< " Zip   " << input._zip << endl;
+		return base;
+	}
+
+	void set(vector<string>& input){ 
+		_ID = stoi(input[0]);
+		_name = input[1]; 
+		_city = input[2];
+		_state = input[3];
+		_zip = input[4];
+	}
+
+	Vendor(vector<string> input){
+		set(input);
+	}
+
+	//bool set_ID(string ID){ _ID = stoi(ID); }
+
+	//void set_ID(int ID){ _ID = ID; }
+	//void set_name(string name){ _name = name; }
+	//void set_city(string city){ _city = city; }
+	//void set_state(string state){ _state = state; }
+	//void set_zip(string zip){ _zip = zip; }
+
+	//int get_ID(){ return _ID; }
+	//string get_name(){ return _name; }
+	//string get_city(){ return _city; }
+	//string get_state(){ return _state; }
+	//string get_zip(){ return _zip; }
+};
+
+
+class IndexNode : public SimpleLinked<Vendor> {
+public:
+	string _state_IndexNode;
+
+	IndexNode(){}
+
+	IndexNode(string state){ _state_IndexNode = state; }
+
+	friend bool operator<(IndexNode& base, IndexNode& input){
+		return base._state_IndexNode < input._state_IndexNode;
+	}
+
+	friend bool operator>(IndexNode& base, IndexNode& input){
+		return base._state_IndexNode > input._state_IndexNode;
+	}
+
+	void operator=(IndexNode& input){
+		clear();
+		set_placer(0);
+		do {
+			insert(input.node_placer->value);
+		} while (increment_placer());
+	}
+
+	friend ostream& operator<<(ostream& base, IndexNode& input){
+
+		input.set_placer(0);
+		do {
+			base << input.node_placer->value;
+		} while (input.increment_placer());
+
+		return base;
+	}
+};
+
+
+class SimpleLinked_Vendor : public SimpleLinked<IndexNode> {
+	//Hide inherited insert
+	SimpleLinked<IndexNode>::insert;
+	bool add_state(IndexNode value){
+		SimpleLinked<IndexNode>::insert(value);
+	}
+
+public:
+
+	SimpleLinked_Vendor(){}
+
+	bool add_vendor(Vendor& input);
+	bool build_list(string filename);
+
+
+
+	friend ostream& operator<<(ostream& base, SimpleLinked_Vendor& input){
+
+		input.set_placer(0);
+		do {
+			base << input.node_placer->value;
+		} while (input.increment_placer());
+
+		return base;
+	}
+};
+
+bool SimpleLinked_Vendor::add_vendor(Vendor& input){
+	set_placer(0);
+
+	bool match = false;
+	do {
+		if (node_placer->value._state_IndexNode == input._state){
+			match = true;
+		}
+	} while (increment_placer() && !match);
+
+	if (!match){
+		insert(*new IndexNode(input._state));
+	}
+
+	//Set node pointer to correct index node
+	set_placer(0);
+	while (input._state != node_placer->value._state_IndexNode && increment_placer()) {}
+
+	node_placer->value.insert(input);
+
+	return true;
+}
+
+bool SimpleLinked_Vendor::build_list(string filename){
+	string line;
+
+	vector<string> prepare_line(string input);
+
+	std::ifstream file_input;
+	file_input.open(filename);
+
+	if (file_input.is_open()){
+		while (!file_input.eof()){
+			vector<string> delimited;
+
+			getline(file_input, line);
+
+			delimited.push_back(line.substr(0, 10));
+			delimited.push_back(line.substr(12, 43));
+			delimited.push_back(line.substr(45, 60));
+			delimited.push_back(line.substr(62, 72));
+			delimited.push_back(line.substr(74, 79));
+
+			node_placer->value.insert(*new Vendor(delimited));
+		}
+	}
+
+	return true;
+}
+
+#endif
